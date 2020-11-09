307679083abcb301cb55ce42bf57b81588e84840
Steve Streeting <steve@stevestreeting.com>
2020-11-09 16:23:23 +0000

    Initial test of importing a FBX file from the command line & making sure no errors

--- UdimUnpack.cpp
+++ UdimUnpack.cpp
@@ -6,13 +6,34 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-	cout << "Hello CMake." << endl;
+	if (argc < 2)
+	{
+		printf("Required: input FBX file name");
+        exit(-1);
+	}
+	const char* filename = argv[1];
 
 
 	// Initialize the SDK manager. This object handles memory management.
-    FbxManager* lSdkManager = FbxManager::Create();
+    FbxManager* sdkManager = FbxManager::Create();
+	
+	// Create the IO settings object.
+    FbxIOSettings *ios = FbxIOSettings::Create(sdkManager, IOSROOT);
+    sdkManager->SetIOSettings(ios);
+
+    // Create an importer using the SDK manager.
+    FbxImporter* importer = FbxImporter::Create(sdkManager,"");
+
+    // Use the first argument as the filename for the importer.
+    if(!importer->Initialize(filename, -1, sdkManager->GetIOSettings())) {
+        printf("Call to FbxImporter::Initialize() failed.\n");
+        printf("Error returned: %s\n\n", importer->GetStatus().GetErrorString());
+        exit(-1);
+    }
+
+	
 	
 	return 0;
 }
